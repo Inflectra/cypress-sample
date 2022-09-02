7f4e07a57437af4a4ac6890cb401411bada7d9e9
Adam Sandman <adam.sandman@inflectra.com>
2022-09-02 11:29:20 -0400

    [RQ:4344] added test case mapping support

--- cypress.config.js
+++ cypress.config.js
@@ -16,11 +16,13 @@ module.exports = defineConfig({
     projectId: 1,
     releaseId: 1,
     testSetId: null,
-    testCaseId: 4,    
     login: 'fredbloggs',
     apiKey: '{7A05FD06-83C3-4436-B37F-51BCF0060483}',
     protocol: 'http',
     host: '127.0.0.1',
-    vdir: 'Spira'
+    vdir: 'Spira',
+    mapping: {
+      "with a checked task": 4
+    }
   }
 });
--- reporter/SpiraReporter.js
+++ reporter/SpiraReporter.js
@@ -28,7 +28,8 @@ function SpiraReporter(runner, options) {
   var restDone = false;
 
   runner.on('start', function () {
-    console.log(color('suite', 'Starting Test Run'));
+    //Log to the console that we're running using this reporter
+    console.log(color('suite', 'Starting Test Run using SpiraTest Reporter'));
   });
 
   runner.on('pass', function(test){
@@ -55,25 +56,39 @@ function SpiraReporter(runner, options) {
   });
 
   runner.on('end', function(){
-    console.log(color('suite', 'Test Run ended with: %d passed, %d failed out of %d test(s).'), passes, failures, passes + failures);
 
-    //Send to SpiraTest
-    var spiraClient = new SpiraClient(self._options.protocol, self._options.host, self._options.port, self._options.vdir, self._options.login, self._options.apiKey);
-    var projectId = self._options.projectId;
-    var testCaseId = self._options.testCaseId;
-    var releaseId = self._options.releaseId;
-    var testSetId = self._options.testSetId;
-    var endDate = new Date();
-    var executionStatusId = 4; /* N/A */
-    var assertCount = failures;
-    var totalCount = passes + failures;
-    var message = passes + ' passed, ' + failures + ' failed out of ' + totalCount + ' test(s).';
-    if (totalCount > 0) {
-      executionStatusId = (failures > 0) ? /* Failed */ 1 : /* Passed */ 2;      
+    //Find the matching test case id from the mapping
+    var testCaseId = null;
+    if (self._options.mapping && self._options.mapping[testName])
+    {
+      testCaseId = parseInt(self._options.mapping[testName]);
     }
-  
-    spiraClient.recordTestRun(projectId, testCaseId, releaseId, testSetId, startDate, endDate, executionStatusId, testName, assertCount, message, stackTrace, steps, self._onRecordSuccess, self._onRecordFailure);
 
+    if (testCaseId)
+    {
+      console.log(color('suite', 'Test Run ended with: %d passed, %d failed out of %d test(s).'), passes, failures, passes + failures);
+      console.log(color('suite', 'Sending results to SpiraTest for test case TC:' + testCaseId));
+
+      //Send to SpiraTest
+      var spiraClient = new SpiraClient(self._options.protocol, self._options.host, self._options.port, self._options.vdir, self._options.login, self._options.apiKey);
+      var projectId = self._options.projectId;
+      var releaseId = self._options.releaseId;
+      var testSetId = self._options.testSetId;
+      var endDate = new Date();
+      var executionStatusId = 4; /* N/A */
+      var assertCount = failures;
+      var totalCount = passes + failures;
+      var message = passes + ' passed, ' + failures + ' failed out of ' + totalCount + ' test(s).';
+      if (totalCount > 0) {
+        executionStatusId = (failures > 0) ? /* Failed */ 1 : /* Passed */ 2;      
+      }
+    
+      spiraClient.recordTestRun(projectId, testCaseId, releaseId, testSetId, startDate, endDate, executionStatusId, testName, assertCount, message, stackTrace, steps, self._onRecordSuccess, self._onRecordFailure);
+    }
+    else
+    {
+      console.log(color('fail', 'No SpiraTest test case ID specified for this test, so it won\'t be reported back to SpiraTest'));
+    }
     //Return exit code
     process.exit(failures);
   });
